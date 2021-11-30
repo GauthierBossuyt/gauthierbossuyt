833fab21b0853774c33502229cbda04f10215723
Gauthier Bossuyt <55983896+GauthierBossuyt@users.noreply.github.com>
2021-11-30 18:45:26 +0100

    scroll-behaviour smooth bad

--- src/components/app.js
+++ src/components/app.js
@@ -7,34 +7,34 @@ let smallWidth = 157;
 let bigWidth = 760;
 
 export default function App() {
-  const surface = useRef(null);
+    const surface = useRef(null);
 
-  let changeWidth = (Q, isOneOpen, library) => {
-    let widthContainer = smallWidth * Q;
-    if (isOneOpen) {
-      widthContainer += bigWidth - smallWidth;
-    }
-    surface.current.style.width = `calc(100vw + ${widthContainer}px)`;
-    library.current.style.width = `${widthContainer}px`;
-    window.scrollBy({ left: widthContainer, behavior: "smooth" });
-  };
+    let changeWidth = (Q, isOneOpen, library) => {
+        let widthContainer = smallWidth * Q;
+        if (isOneOpen) {
+            widthContainer += bigWidth - smallWidth;
+        }
+        surface.current.style.width = `calc(100vw + ${widthContainer}px)`;
+        library.current.style.width = `${widthContainer}px`;
+        window.scrollBy({ left: widthContainer, behavior: "smooth" });
+    };
 
-  let test = (event) => {
-    if (event.deltaY !== 0) {
-      window.scrollBy({ left: event.deltaY * 1.5, behavior: "smooth" });
-    }
-    if (event.deltaX !== 0) {
-      window.scrollBy({ left: event.deltaX * 1.5, behavior: "smooth" });
-    }
-  };
+    let test = (event) => {
+        if (event.deltaY !== 0) {
+            window.scrollBy({ left: event.deltaY });
+        }
+        if (event.deltaX !== 0) {
+            window.scrollBy({ left: event.deltaX });
+        }
+    };
 
-  return (
-    <div className="surface" ref={surface} onWheelCapture={test}>
-      <div className="content">
-        <Navigation />
-        <Welcome />
-      </div>
-      <Library onResize={changeWidth} />
-    </div>
-  );
+    return (
+        <div className="surface" ref={surface} onWheelCapture={test}>
+            <div className="content">
+                <Navigation />
+                <Welcome />
+            </div>
+            <Library onResize={changeWidth} />
+        </div>
+    );
 }
